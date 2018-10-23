da31d0dba8b5efc6a4e40002d85d00a41b28cebf
Neeraj <neeraj@CPU1588.local>
2018-10-23 12:16:51 +0530

    update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+	console.log('App started')
   res.sendFile(__dirname + "/index.html");
 });
 
